66b8952283ca071017546f52e1e3e5c8a775ebbb
gengesh <gengeswarancse11@gmail.com>
2023-08-14 14:41:33 +0530

    dom manipulation task 12

---
+++ dom12/dom12.js
@@ -0,0 +1,25 @@
+var form = document.getElementById('addForm');
+
+
+
+//add listener to add button
+form.addEventListener("submit",addUser);
+
+
+
+
+function addUser(e){
+    e.preventDefault();
+    var uName = document.getElementById('uname').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    // console.log(uName,email);
+    var li = document.createElement('li');
+    li.className= "list-group-item p-1 mt-2";
+    var listUsers = document.getElementById('listUsers');
+    var content = uName+" : "+email+" : "+phone;
+    li.appendChild(document.createTextNode(content));
+    console.log(li);
+    listUsers.appendChild(li);
+localStorage.setItem(email,uName);
+}
