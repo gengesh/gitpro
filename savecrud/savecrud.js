085b51d8c7a7b916b2c6eb245fa8d2c1e3e75570
gengesh <gengeswarancse11@gmail.com>
2023-09-08 09:57:33 +0530

    save in crudcurd

---
+++ savecrud/savecrud.js
@@ -0,0 +1,95 @@
+var form = document.getElementById('addForm');
+var listUsers = document.getElementById('listUsers');
+function allStorage() {
+    var archive = [];
+    for (var i = 0; i<localStorage.length; i++) {
+        archive[i] = localStorage.getItem(localStorage.key(i));
+    //    console.log(archive[i]);
+       let user = JSON.parse(archive[i]);
+    //    console.log(user.uName);
+        // console.log(uName,email);
+        var li = document.createElement('li');
+        li.className= "list-group-item p-1 mt-2";
+        var content = user.uName+" : "+user.email+" : "+user.phone;
+        li.appendChild(document.createTextNode(content));
+        // console.log(li);
+        var button = document.createElement('button');
+        button.className="btn btn-danger float-right btn-sm delete";
+        button.appendChild(document.createTextNode('delete'));
+        var editButton = document.createElement('button');
+        editButton.className="btn float-right btn-sm mr-3 edit";
+        editButton.appendChild(document.createTextNode("Edit"));
+        li.appendChild(button);
+        li.appendChild(editButton);
+        listUsers.appendChild(li);
+    }
+}
+allStorage();
+//add listener to add button
+form.addEventListener("submit",addUser);
+
+listUsers.addEventListener("click",deleteUser);
+
+
+function addUser(e){
+    e.preventDefault();
+    var uName = document.getElementById('uname').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    // console.log(uName,email);
+    var li = document.createElement('li');
+    li.className= "list-group-item p-1 mt-2";
+    var content = uName+" : "+email+" : "+phone;
+    li.appendChild(document.createTextNode(content));
+    // console.log(li);
+    var button = document.createElement('button');
+    button.className="btn btn-danger float-right btn-sm delete";
+    button.appendChild(document.createTextNode('delete'));
+    var editButton = document.createElement('button');
+    editButton.className="btn float-right btn-sm mr-3 edit";
+    editButton.appendChild(document.createTextNode("Edit"));
+    li.appendChild(button);
+    li.appendChild(editButton);
+    listUsers.appendChild(li);
+    const obj = {
+        uName,
+        email,
+        phone
+    }
+    axios.post('https://crudcrud.com/api/3ce20d25f37b4c8eb0877c9a674703dd/userData',obj)
+    .then((response) => {
+        console.log(response)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+//     var obj_serialized = JSON.stringify(obj);
+// localStorage.setItem(obj.email,obj_serialized);
+}
+function deleteUser(e){
+    // e.preventDefault();
+   if(e.target.classList.contains('delete')){
+    if(confirm("cofirm delete the user")){
+        let li=e.target.parentElement;
+        let obj1=li.firstChild.data;
+        // console.log(obj1);
+        let str=obj1.split(" : ");
+        // console.log(str[1]);
+        localStorage.removeItem(str[1]);
+        listUsers.removeChild(li);
+        // localStorage.removeItem(obj.email);
+
+     }
+   }else if(e.target.classList.contains('edit'))
+{
+    // console.log("enter into edit");
+    let li=e.target.parentElement;
+    let obj1=li.firstChild.data;
+    let str=obj1.split(" : ");
+    document.getElementById('uname').value=str[0];
+    document.getElementById('email').value=str[1];
+    document.getElementById('phone').value=str[2];
+    localStorage.removeItem(str[1]);
+    listUsers.removeChild(li);
+
+}}
