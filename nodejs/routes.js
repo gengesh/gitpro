ef58685cd535a615fb3873ff3762d479a01b8bff
gengesh <gengeswarancse11@gmail.com>
2023-09-24 08:45:05 +0530

    nodemon restart

--- nodejs/routes.js
+++ nodejs/routes.js
@@ -42,7 +42,8 @@ const requestHandler = (req,res) => {
            res.write('<head><title>My first page:</title></head>');
            res.write('<body><h1>Hello from my Node.js Server</h1></body>');
            res.write('</html>');
-           res.end();
+           res.end();  
+           
     }
 }
 // module.exports = requestHandler;
