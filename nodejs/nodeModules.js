66ba5aa43b2d58a4b5b6df326a127137b878fe3a
gengesh <gengeswarancse11@gmail.com>
2023-09-26 15:17:39 +0530

    express js start

--- nodejs/nodeModules.js
+++ nodejs/nodeModules.js
@@ -1,5 +1,14 @@
 const http = require('http');
-const routes = require('./routes');
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const express = require('express');
+const app = express();
+app.use((req,res,next) =>{
+    console.log("in the Middleware");
+    next();//allows the request to continue to the next middleware in line
+});
+app.use((req,res,next) =>{
+    console.log("in another Middleware");
+
+    res.send("<h1>Hello form Express!</h1>");
+});
+const server = http.createServer(app);
     server.listen(4000);
