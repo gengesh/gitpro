13006e99dc6369f984ac8ee0dee973570a57ae4f
gengesh <gengeswarancse11@gmail.com>
2023-09-28 08:25:51 +0530

    Routers and Filters

--- nodejs/nodeModules.js
+++ nodejs/nodeModules.js
@@ -1,19 +1,16 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next) =>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin',adminRoutes);
+app.use(shopRoutes); 
+app.use((req,res,next) => {
+    res.status(404).send('<h1> Page not found!</h1>');
 })
-app.use('/',(req,res,next) =>{
-    res.send("<h1>Hello form Express!</h1>");
-});
-const server = http.createServer(app);
-    server.listen(4000);
+// const server = http.createServer(app);
+    app.listen(4000);
