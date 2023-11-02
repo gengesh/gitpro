674ca45dc6b7a9963f4f81e928cef9833a707b59
gengesh <gengeswarancse11@gmail.com>
2023-11-02 10:13:44 +0530

    user's password at risk expense tracker

--- ExpenseTrackerApp/backEnd/controllers/login.js
+++ ExpenseTrackerApp/backEnd/controllers/login.js
@@ -1,5 +1,5 @@
 const Users = require('../models/users.js');
-
+const bcrypt = require('bcrypt');
 
 exports.postLogin = async (req,res,next) => {
     console.log("this is constroller req",req.body);
@@ -10,17 +10,21 @@ exports.postLogin = async (req,res,next) => {
     })
     .then((response) =>{
         if(response){
-           if(response.password == password){
-            res.status(200).json({message:"200"});
+           bcrypt.compare(password,response.password,(err,result) =>{
+            if(err){
+                res.status(200).json({message:"404"});
+            }else if(result){
+                res.status(200).json({message:"200"});
+            }else{
+                res.status(200).json({message:"401"});
+               }
+           }) 
            }else{
-            res.status(200).json({message:"401"});
-           }
-        }else{
             res.status(200).json({message:"404"});
-        }
-    })
-    .catch(err => {
-       console.log(err);
-    });
+           }
+        })
+        .catch(err =>{
+            res.status(500).json({message:"something went wrong!"});
+        })
     
 }
--- ExpenseTrackerApp/backEnd/controllers/signup.js
+++ ExpenseTrackerApp/backEnd/controllers/signup.js
@@ -1,21 +1,33 @@
 const Users = require('../models/users.js');
-
+const bcrypt = require('bcrypt');
 
 exports.postSignup = async (req,res,next) => {
     console.log("this is constroller req",req.body);
     const name = req.body.name;
     const email = req.body.email;
     const password = req.body.password;
+    bcrypt.genSalt(10, (err,salt) => {
+        bcrypt.hash(password,salt,async(err,hash) =>{
+            if(err){
+                res.status(500).json({success:false,message:"something went wrong!"});
+            }else{
+               
     await Users.create({
         name:name,
         email:email,
-        password:password,
+        password:hash,
     })
     .then(response =>{
         res.status(201).json({info:"successfully created."});
     })
     .catch(err => {
+        console.log(err);
         res.status(200).json({info:"email id already exists!"});
     });
+            }
+        })
+    })
+
+
     
 }
